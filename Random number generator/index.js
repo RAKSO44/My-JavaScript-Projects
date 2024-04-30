acae773dbbf0e8314363c0f4edd741b71aa188ad
RAKSO44 <oscar_cuco4@hotmail.com>
2024-04-30 18:40:00 -0500

    Random number generator

---
+++ Random number generator/index.js
@@ -0,0 +1,21 @@
+// RANDOM NUMBER GENERATOR CODE
+/* ------------------------------------------------------- */
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum1;
+let randomNum2;
+let randomNum3;
+
+myButton.onclick = function(){
+    randomNum1 = Math.floor(Math.random() * max) + min;
+    randomNum2 = Math.floor(Math.random() * max) + min;
+    randomNum3 = Math.floor(Math.random() * max) + min;
+    label1.textContent = randomNum1;
+    label2.textContent = randomNum2;
+    label3.textContent = randomNum3;
+}
+
+/* ------------------------------------------------------- */ 
